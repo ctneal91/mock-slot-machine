f749109908cd09f89f858b08db5d286edfc32905
ctneal91 <ctnealherman@gmail.com>
2025-12-23 10:03:22 -0600

    Fix CORS config to handle nil FRONTEND_URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "http://127.0.0.1:3001", ENV["FRONTEND_URL"]
+    origins(*[ "http://localhost:3001", "http://127.0.0.1:3001", ENV["FRONTEND_URL"] ].compact)
 
     resource "*",
       headers: :any,
